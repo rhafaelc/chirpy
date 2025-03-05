abf80e45282134786f8d0034bb1e1f7a30da27a3
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-03-05 02:33:38 +0000

    feat: validate chirp length

--- main.go
+++ main.go
@@ -23,9 +23,11 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
 
 	srv := &http.Server{
 		Addr:    ":" + port,
